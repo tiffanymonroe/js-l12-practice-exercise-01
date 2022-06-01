0319a427a702f9f714581bf91ed7b66a06e303b9
Tiffany Monroe <13427358+tiffanymonroe@users.noreply.github.com>
2022-06-01 08:46:55 -0700

    random user generator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+  const usersRequest = await fetch ("https://randomuser.me/api?results=5");
+  const data = await usersRequest.json();
+  const userResults = data.results;
+  displayUsers(userResults);
+};
+
+const displayUsers = function (userResults) {
+  randomFolks.innerHTML = "";
+
+  for (let user of userResults) {
+    const country = user.location.country;
+    const name = user.name.first;
+    const imageUrl = user.picture.medium;
+    const userDiv = document.createElement("div");
+    userDiv.innerHTML = `
+      <h3>${name}</h3>
+      <p>${country}</p>
+      <img src=${imageUrl} alt="User avatar" />
+    `;
+    randomFolks.append(userDiv);
+  }
+}
+
+getData();
